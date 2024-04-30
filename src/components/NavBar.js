00c926ef8dee41f2c7d3e45c17132ef8e5f5b4b9
whitney <shisiawhitney215@gmail.com>
2024-04-30 22:02:33 +0300

    react

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import {NavLink} from 'react-router-dom';
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        /* add styling to Navlink */
+        className="nav-link"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className="nav-link"
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        className="nav-link"
+      >
+        Login
+      </NavLink>
+    </nav>
+  )
+}
+
+export default NavBar
